36c1d1314734b48c0e5d67ce450cdcbdb92bfe85
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-03-01 23:54:13 +0100

    Removed dependency on requests, in favor of aiohttp

--- custom_commands.py
+++ custom_commands.py
@@ -1,6 +1,6 @@
 import asyncio
 import discord
-import requests
+import aiohttp
 from discord.ext import commands
 
 class CustomCommands(commands.Cog):
@@ -9,6 +9,15 @@ class CustomCommands(commands.Cog):
 
     @commands.command()
     async def quote(self, ctx):
-        r = requests.get('http://www.bzl.no/api/quotes.php')
-        j = r.json()
-        await ctx.send('{0} -- {1}'.format(j['text'], j['from']))
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://www.bzl.no/api/quotes.php') as r:
+                if r.status == 200:
+                    json = await r.json()
+                    await ctx.send('{0} -- {1}'.format(json['text'], json['from']))
+        # r = requests.get('http://www.bzl.no/api/quotes.php')
+        # j = r.json()
+        # await ctx.send('{0} -- {1}'.format(j['text'], j['from']))
+
+    @commands.command()
+    async def pm(self, ctx):
+        await ctx.message.author.send('Hi there')
