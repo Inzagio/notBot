c8831e56a020dc61fe4470d430f6678b8507b005
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-03-01 23:26:08 +0100

    Added a cog (commands grouped into class), and utilization of it

--- app.py
+++ app.py
@@ -2,33 +2,14 @@ import discord
 import asyncio
 import config
 from discord.ext import commands
+from custom_commands import CustomCommands
 
-# from commandsModule import CommandsTest
+bot = commands.Bot(command_prefix='$')
 
-client = discord.Client()
-
-bot = commands.Bot(command_prefix='!')   
-
-@bot.command()
-async def what(ctx):
-    pass
-
-
-@bot.command()
-async def testArgs(ctx, arg1, arg2):
-    await ctx.send('You passed {} and {}'.format(arg1, arg2))
-
-
-@client.event # event decorator/wrapper
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
 
-@client.event
-async def on_message(message):
-    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content} ")
-    
-    if "hi there" in message.content.lower():
-        await message.channel.send(f"Hi! {message.author.name}")
-
-
-client.run(config.token)
+bot.add_cog(CustomCommands(bot))
+bot.run(config.token)
