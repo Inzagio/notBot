e0d6eec70a8e8f6af5abc45aaacd28c85c8f1c85
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-03-02 01:09:52 +0100

    Removed old/unused files

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import discord
 import asyncio
 import config
 from discord.ext import commands
-from custom_commands import CustomCommands
 
 bot = commands.Bot(command_prefix='$')
 
@@ -10,5 +9,3 @@ bot.load_extension("cogs.events")
 bot.load_extension("cogs.commands")
 
 bot.run(config.token)
-
-# bot.change_presence(activity=discord.Game(name='World of Bots'))
